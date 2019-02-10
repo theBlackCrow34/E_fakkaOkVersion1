23a5945551353a02586f15f6011060a905a936e0
theBlackCrow34 <44416687+theBlackCrow34@users.noreply.github.com>
2019-02-10 16:41:34 +0200

    version1

--- app/src/main/java/com/example/sherif/e_fakkaokversion1/MainActivity.java
+++ app/src/main/java/com/example/sherif/e_fakkaokversion1/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.sherif.e_fakkaokversion1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private Button sign_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sign_in = (Button) findViewById(R.id.signin);
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/sherif/e_fakkaokversion1/SplashScreen.java
+++ app/src/main/java/com/example/sherif/e_fakkaokversion1/SplashScreen.java
@@ -1,14 +1,29 @@
 package com.example.sherif.e_fakkaokversion1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+        imageView = (ImageView) findViewById(R.id.image_splash);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
